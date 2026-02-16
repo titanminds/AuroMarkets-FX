24e81b5235373a39cd7205057077e6cd2697c0b8
titanminds <githubtitanminds@gmail.com>
2026-02-16 15:31:32 +0500

    changes done 2

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,10 +1,17 @@
-import FAQSection from '../../components/FAQS/FAQSection'
-import React from 'react'
+import PageHero from "../../components/About/Hero"
+import FAQSection from "../../components/FAQS/FAQSection"
+import React from "react"
 
 const page = () => {
   return (
     <div>
-      <FAQSection/>
+      <PageHero
+        title="FAQ's"
+        description="Welcome to AuroMarkets FX, a leading online forex company. We empower traders with the tools, expertise and support needed to succeed in the global markets."
+        backgroundImage="https://static.vecteezy.com/system/resources/thumbnails/052/042/795/small/trading-forex-gold-or-xauusd-char-growth-up-profit-in-bullish-market-vector.jpg"
+        breadcrumbs={[{ label: "Home", href: "/" }, { label: "FAQ's" }]}
+      />
+      <FAQSection />
     </div>
   )
 }
