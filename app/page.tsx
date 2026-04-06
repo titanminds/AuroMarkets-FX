a353bff653614b28982f8dde7bc8815b2de4f6f3
titanminds <githubtitanminds@gmail.com>
2026-04-06 14:12:55 +0500

    remove ticker below hro.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <TradingViewTicker />
+      {/* <TradingViewTicker /> */}
       <HangingCards />
       <PricingList />
       <Ecosystem />
